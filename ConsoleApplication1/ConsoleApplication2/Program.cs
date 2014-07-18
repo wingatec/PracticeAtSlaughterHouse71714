7f004839c092b9eda68108895dad7f10fa1d074f
wingatec <carmelitawingate@gmail.com>
2014-07-17 20:19:27 -0400

    1.0

---
+++ ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //have the user pick a number from 1-20
+            var numbers = new List<int>() { 9, 8, 12, 11, 4, 3, 17, 13, 20 };
+
+
+            Console.WriteLine("Pick a number between 1 and 20");
+            int userInput = int.Parse(Console.ReadLine());
+
+
+            var comp = numbers.Contains(userInput);
+
+           if (comp)
+           {
+               Console.WriteLine("Hooray! You're correct! :-) ");
+               Console.ReadLine();
+           }
+            
+
+           else
+           {
+               Console.WriteLine("Oh Noooooo! Wrong number :-(");
+               Console.ReadLine();
+           }
+
+
+
+          
+        }
+    }
+}
---
+++ PracticeAtSlaughterHouse71714.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceAtSlaughterHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new List<int>() { 83, 76, 12, 8, 22, 4, 6, 76, 54, 58 };
+            //create a list of integers, fill with number 1-100use linq to find largest value in the list and display in console
+            foreach (var item in num) { Console.WriteLine(item); }
+            var BigOne = num.Max();
+
+            
+
+            Console.WriteLine("The largest number is {0}", BigOne);
+            Console.ReadLine();
+
+
+
+////
+        }
+
+
+
+			
+}
+
+}
+
