b5be8c94ae77b656e4c1c91c07d529bbd4997576
wingatec <carmelitawingate@gmail.com>
2014-07-17 20:45:31 -0400

    1.2

---
+++ ConsoleApplication1/ConsoleApplication4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //create a sentence and reverse it in the console
+
+            var words = new List<string>() {See, if, you, can, print, this, backwards};
+
+
+        }
+    }
+}
